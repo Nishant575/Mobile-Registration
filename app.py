eae31f1559825ff591636c4a8f674504861d3e39
Nishant575 <n.dalvi2010@gmail.com>
2021-02-17 13:41:19 +0530

    Backend done

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import Text
+from tkinter import messagebox
 
 wind = Tk()
 wind.geometry("700x800")
@@ -8,6 +10,57 @@ wind.title("Mobile Registration")
 # --variables --
 nm = StringVar()
 mbn = StringVar()
+
+# --Function to insert data in text file--
+def insert():
+
+    k = 0
+    name = nm.get()
+    number = mbn.get()
+
+    if name == "" or number == "":
+        messagebox.showerror("Error", "Please enter info")
+    elif len(number) > 10:
+        messagebox.showerror("Error", "Invalid Mobile Number")
+        inp_mbn.delete(0, 'end')
+    else:
+        data_file = open("data.txt","r")
+        for val in data_file.readlines():
+            if number in val:
+                k = 1
+                break
+        data_file.close()
+        if k == 1:
+            messagebox.showerror("Error", "Number already exists")
+            inp_mbn.delete(0, 'end')
+        else:
+            data_file = open("data.txt","a")
+            str = name +" "+ "--->"+ " " + number + "\n"
+            data_file.write(str)
+            inp_mbn.delete(0, 'end')
+            inp_name.delete(0, 'end')
+            messagebox.showinfo("Successfull", "Registered Successfully")
+            data_file.close()
+
+# --Function to display data if present--
+def check():
+    disp_data.delete("1.0","end")
+    data_file = open("data.txt","r")
+    for val in data_file.readlines():
+        disp_data.insert(tk.END,val+"\n")
+    if len(disp_data.get("1.0", "end-1c")) == 0:
+        messagebox.showerror("Data not found", "No data to show")    
+
+
+# --Function to clear all text fields--
+def clear():
+    disp_data.delete("1.0","end")
+    inp_mbn.delete(0, 'end')
+    inp_name.delete(0, 'end')
+
+# --Function to close application--
+def close():
+    wind.destroy()
 
 # --Design --
 lbl_head = Label(wind, font=( 'aria' ,28, 'bold' ),text="Mobile Registration",fg="steel blue",bd=10)
@@ -26,15 +79,15 @@ lbl_name =  Label(f1, font=( 'aria' ,20, ),text="Name : ",fg="gray45")
 lbl_name.place(x=80,y=0)
 
 inp_name = Entry(f1,font=( 'aria' ,16, ),bg="LightYellow",width=25,textvar=nm)
-inp_name.place(x=170,y=0)
+inp_name.place(x=210,y=10)
 
 lbl_mbn =  Label(f1, font=( 'aria' ,20, ),text="Mobile Number : ",fg="gray45")
 lbl_mbn.place(x=0,y=70)
 
 inp_mbn = Entry(f1,font=( 'aria' ,16, ),bg="LightYellow",width=25,textvar=mbn)
-inp_mbn.place(x=170,y=70)
+inp_mbn.place(x=210,y=80)
 
-btn_insert = Button(f1,text="Insert",width=10,bg="red")
+btn_insert = Button(f1,text="Insert",width=10, command=insert)
 btn_insert.place(x=200,y=130)
 
 # --Print Frame---
@@ -47,12 +100,13 @@ lbl_vinf.place(x=0,y=0)
 disp_data = Text(f2,font=( 'aria' ,16, ), width=59, height=13,bg='AliceBlue')
 disp_data.place(x=0,y=40)
 
-btn_Check = Button(f2,text="Check",width=10)
-btn_Check.place(x=350,y=300)
+btn_Check = Button(f2,text="Check",width=10,command=check)
+btn_Check.place(x=350,y=360)
 
-btn_Clear = Button(f2,text="Clear",width=10)
-btn_Clear.place(x=480,y=300)
+btn_Clear = Button(f2,text="Clear",width=10,command=clear)
+btn_Clear.place(x=480,y=360)
 
-
+btn_Check = Button(f2,text="Exit",width=10,bg="red",command=close)
+btn_Check.place(x=200,y=360)
 
 wind.mainloop()
